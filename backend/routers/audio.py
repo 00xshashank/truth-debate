76ccab0b58a1ada168ac366a2ba665d042cd737e
00xshashank <shashank.krishnamani@gmail.com>
2026-07-12 12:58:02 +0530

    Change to io.BytesIO instead of writing and reading to file

--- backend/routers/audio.py
+++ backend/routers/audio.py
@@ -5,6 +5,7 @@ import os
 import requests
 from loguru import logger
 import json
+import io
 
 TRANSCRIPTION_ENDPOINT = "https://api.sarvam.ai/speech-to-text"
 AUDIO_DIR="audio"
@@ -20,20 +21,17 @@ def transcribe_request(file: Annotated[bytes, File()]):
     id = str(uuid4())
     filename = f"{AUDIO_DIR}/{id}"
 
-    with open(f"{filename}.webm", "wb+") as f:
-        f.write(file)
-
-    with open(f"{filename}.webm", "rb") as f:
-        response = requests.post(
-            TRANSCRIPTION_ENDPOINT,
-            files={
-                "file": (f"audio-{id}.webm", f, "audio/webm")
-            },
-            headers={
-                "api-subscription-key": SARVAM_API_KEY
-            }
-        )
-        logger.info(f"Received transcription response: {response.text}")
+    byte_stream = io.BytesIO(file)
+    response = requests.post(
+        TRANSCRIPTION_ENDPOINT,
+        files={
+            "file": (f"audio.webm", byte_stream, "audio/webm")
+        },
+        headers={
+            "api-subscription-key": SARVAM_API_KEY
+        }
+    )
+    logger.info(f"Received transcription response: {response.text}")
     
     jres = json.loads(response.text)
     logger.info(f"From validated object: id: {jres['request_id']}, transcript: {jres['transcript']}")
